a1db5d176abcb5055e92351b9e0404010fee1f7e
soby <vthquddbsv@gmail.com>
2024-12-15 16:57:32 +0900

    생성자 오버로딩 , this()

--- java-start/construct/MemberConstruct.java
+++ java-start/construct/MemberConstruct.java
@@ -5,6 +5,15 @@ public class MemberConstruct {
     int age;
     int grade;
 
+//    멤버변수 추가
+    MemberConstruct(String name, int age){
+//        this.name = name;
+//        this.age = age;
+//        this.grade  = 50;
+//        위 코드 단축변경가능
+        this(name,age,50); //변경(*항상 첫줄에만 사용가능*)
+    }
+//   MemberConstruct(String name, int age) 호출후 -> MemberConstruct(String name,int age ,int grade)
     MemberConstruct(String name,int age ,int grade){
         System.out.println("생성자 호출 name = "+name+",age = "+ age+",grade = "+grade);
         this.name=name;
