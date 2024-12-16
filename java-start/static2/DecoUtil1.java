9518212ef8ef3495da7f196fff274d3953988758
soby <vthquddbsv@gmail.com>
2024-12-16 22:15:10 +0900

    static메서드- 클래스 메서드

---
+++ java-start/static2/DecoUtil1.java
@@ -0,0 +1,9 @@
+package static2;
+
+public class DecoUtil1 {
+    public String deco(String str){
+        String result = "*"+str+"*";
+        return result;
+//        return "*"+str+"*"; // 이렇게 함축가능
+    }
+}
---
+++ java-start/static2/DecoUtil2.java
@@ -0,0 +1,10 @@
+package static2;
+
+public class DecoUtil2 {
+
+    public static String deco(String str){
+        String result = "*"+str+"*";
+        return result;
+//        return "*"+str+"*"; // 이렇게 함축가능
+    }
+}
