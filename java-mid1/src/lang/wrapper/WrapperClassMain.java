9186fbf7d1ab0babd08f01300e476bb23ec14e6c
soby <vthquddbsv@gmail.com>
2024-12-31 20:35:58 +0900

    래퍼클래스 자바래퍼클래스 내용추가

--- java-mid1/src/lang/wrapper/WrapperClassMain.java
+++ java-mid1/src/lang/wrapper/WrapperClassMain.java
@@ -2,7 +2,7 @@ package lang.wrapper;
 
 public class WrapperClassMain {
     public static void main(String[] args) {
-        Integer newInteger = new Integer(10);// 빨간줄이 나오는 이유: 미래에 삭제예정, 대신에 valueOf()를 사용
+        Integer newInteger = Integer.valueOf(10);// 빨간줄이 나오는 이유: 미래에 삭제예정, 대신에 valueOf()를 사용
         Integer integerObj = Integer.valueOf(10);//-128 ~ 127 자주 사용하는 숫자 값 재사용,불변
         Long longObj = Long.valueOf(100);
         Double doubleObj = Double.valueOf(10.5);
@@ -20,9 +20,8 @@ public class WrapperClassMain {
         System.out.println("longValue = " + longValue);
 
         System.out.println("비교");
-        System.out.println("==: "+(newInteger == integerObj));
-        System.out.println(".equals(): "+(newInteger.equals(integerObj) ));
-
+        System.out.println("==: " + (newInteger == integerObj)); // 서로 다른 참조를 보고있기때문에 false값반환
+        System.out.println(".equals(): " + (newInteger.equals(integerObj)));
 
 
     }
